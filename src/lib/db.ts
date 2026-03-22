ba2e5eb3e12aaa83e6bdc59550477736322cb6f5
Dhruvin-04 <chudasamadhruvin28@gmail.com>
2026-03-22 23:05:18 +0530

    first commit

---
+++ src/lib/db.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const mongoDBUrl = process.env.MONGODB_URL
+
+if(!mongoDBUrl){
+    throw new Error("db url not found")
+}
+
+let cached = global.mongooseConn
+
+if(!cached){
+    cached = global.mongooseConn = {conn: null, promise: null}
+}
+
+const connnectDB = async () => {
+    if(cached.conn) return cached.conn
+    if(!cached.promise){
+        cached.promise = mongoose.connect(mongoDBUrl).then(c=>c.connection)
+    }
+
+    try {
+        const conn = await cached.promise
+        return conn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connnectDB
